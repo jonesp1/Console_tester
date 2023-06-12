d27e89b2f8be15fdfa3cafa301c97f0ed6287f19
Jonas Poulsen <jonas.poulsen@lego.com>
2023-06-12 13:45:24 +0200

    Implemented the Solution class and the main program for running the test of the solution

--- console_tester/Program.cs
+++ console_tester/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿class Program
+{
+
+    static void Main()
+    {
+        Solution solution = new Solution();
+        Console.WriteLine("Solution Testing");
+        Console.WriteLine("------------------------");
+
+        Console.WriteLine("Test Case 1:");
+        int input1 = 5;
+        int input2 = 3;
+        int result1 = solution.MyQuestion(input1,input2);
+        Console.WriteLine($"Result: {result1}");
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+        
+    }
+    class Solution
+    {
+        public int MyQuestion(int i1, int i2)
+        {
+            //implement here
+            int result = i1 + i2;
+            return result;
+        }
+    }
+}
