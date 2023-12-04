76090159b2d0d4abb503ddfdba426f965d66cdf1
Jonas Poulsen <jonaspoulsen_@hotmail.com>
2023-12-04 08:39:57 +0100

    Added gitignore, and created solution for Add two numbers

--- console_tester/Program.cs
+++ console_tester/Program.cs
@@ -1,29 +1,75 @@
 ﻿class Program
 {
-
     static void Main()
     {
         Solution solution = new Solution();
         Console.WriteLine("Solution Testing");
         Console.WriteLine("------------------------");
 
+        // Test Case 1
         Console.WriteLine("Test Case 1:");
-        int input1 = 5;
-        int input2 = 3;
-        int result1 = solution.MyQuestion(input1,input2);
-        Console.WriteLine($"Result: {result1}");
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode result1 = solution.AddTwoNumbers(l1, l2);
+        PrintListNode(result1);
+
+        // Add more test cases if needed...
 
         Console.WriteLine("Press any key to exit.");
         Console.ReadKey();
-        
     }
-    class Solution
+
+    static void PrintListNode(ListNode node)
     {
-        public int MyQuestion(int i1, int i2)
+        while (node != null)
         {
-            //implement here
-            int result = i1 + i2;
-            return result;
+            Console.Write(node.val + " ");
+            node = node.next;
         }
+        Console.WriteLine();
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode dummyHead = new ListNode();
+        ListNode current = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null)
+        {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int sum = x + y + carry;
+            carry = sum / 10;
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        if (carry > 0)
+        {
+            current.next = new ListNode(carry);
+        }
+
+        return dummyHead.next;
     }
 }
